6e4c072e24a0334907b8f0a17af9cd72fcce720f
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-23 15:46:46 +0530

    Update server.go

--- server.go
+++ server.go
@@ -3,12 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func main() {
-	PORT := os.Getenv("PORT")
-
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
 	err := http.ListenAndServe(":80", nil)
