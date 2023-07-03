8e80ff4ef11692c64c6f779fa7f2939613bed708
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-07-03 17:00:24 +0530

    database setup

--- server.go
+++ server.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	db "app/postgres"
 	"fmt"
 	"net/http"
 )
 
+func getDrivers(w http.ResponseWriter, req *http.Request) {
+	rows, err := db.Connection.Query("SELECT name FROM drivers")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	data := ""
+	for rows.Next() {
+		var name string
+		err = rows.Scan(&name)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(name)
+		data += fmt.Sprintf("%s ", name)
+	}
+	err = rows.Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Fprintf(w, data)
+	rows.Close()
+}
+
 func main() {
+	db.InitDB()
+	defer db.Connection.Close()
+
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/drivers", getDrivers)
 
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
