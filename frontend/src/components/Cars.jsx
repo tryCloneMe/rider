0cd1edbbfa814b10d1c008b7be4287555eb7b496
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-07-22 15:44:56 +0530

    fix: frontend setup

---
+++ frontend/src/components/Cars.jsx
@@ -0,0 +1,131 @@
+import { useEffect, useState } from "react";
+import carImg from "../assets/car.png";
+import {
+  getNextCoordIndex,
+  advanceCoord,
+  countTurns,
+  finalRotation,
+  getTurnDistance,
+} from "../utils/movement";
+import { wait } from "../utils/wait";
+import { fetchInterval, refreshInterval } from "../utils/test";
+
+// const Cars = ({ next, rotation, path }) => {
+const Cars = (props) => {
+  const [position, setPosition] = useState(props.next);
+  const [rotation, setRotation] = useState(finalRotation(props.path, 1));
+  const [rotateBusy, setRotateBusy] = useState(false);
+
+  useEffect(() => {
+    setRotateBusy(false);
+    setPosition(props.next);
+    setRotation(finalRotation(props.path, 1));
+  }, [props]);
+
+  const rotate = async (section, i) => {
+    setRotateBusy(true);
+
+    let myRotation = rotation;
+    const targetRotation = finalRotation(section, i);
+    if (rotation === targetRotation) {
+      setRotateBusy(false);
+      return;
+    }
+
+    const { Clockwise, AntiClockwise } = getTurnDistance(
+      myRotation,
+      targetRotation
+    );
+    const isClockwise = Clockwise < AntiClockwise;
+
+    const diff = Math.min(Clockwise, AntiClockwise);
+    const steps =
+      (refreshInterval * 8) /
+      refreshInterval; /*(turnDuration = refreshInterval * 8)*/
+    const increment = diff / steps;
+
+    while (rotation !== targetRotation) {
+      if (isClockwise) myRotation += increment;
+      else myRotation -= increment;
+
+      if (myRotation > 360) myRotation = 0;
+      else if (myRotation < 0) myRotation = 360 - Math.abs(myRotation);
+
+      setRotation(myRotation);
+      await wait(refreshInterval);
+    }
+
+    setRotateBusy(false);
+  };
+
+  const move = async (next) => {
+    let [currX, currY] = position;
+
+    const startIndex = getNextCoordIndex(currX, currY, props.path);
+    const endIndex = props.path.findIndex(([x, y]) => {
+      return x === next[0] && y === next[1];
+    });
+
+    const section = props.path.slice(startIndex, endIndex + 1);
+    const myCountTurns = countTurns(section);
+    const myTurnDuration =
+      myCountTurns *
+      refreshInterval *
+      8; /*(turnDuration = refreshInterval * 8)*/
+
+    const distance = endIndex - startIndex + Math.max(currX % 1, currY % 1);
+    const steps = (fetchInterval - myTurnDuration) / refreshInterval;
+    const increment = distance / steps;
+
+    for (let i = 0; i < section.length; i++) {
+      if (i > 0) {
+        while (rotateBusy) {
+          await wait(refreshInterval);
+        }
+        await rotate(section, i);
+      }
+      const [nextX, nextY] = section[i];
+
+      while (currX !== nextX) {
+        if (next !== props.next) return;
+
+        currX = advanceCoord(currX, nextX, increment);
+        setPosition((prev) => [currX, prev[1]]);
+        await wait(refreshInterval);
+      }
+
+      while (currY !== nextY) {
+        if (next !== props.next) return;
+
+        currY = advanceCoord(currY, nextY, increment);
+        setPosition((prev) => [prev[0], currY]);
+        await wait(refreshInterval);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (props.next !== position) {
+      move(props.next);
+    }
+  }, [props.next]);
+
+  const [x, y] = position;
+
+  return (
+    <div
+      style={{
+        width: "40px",
+        height: "40px",
+        position: "absolute",
+        top: `${x}px`,
+        left: `${y}px`,
+        transform: `rotate(${rotation}deg)`,
+      }}
+    >
+      <img src={carImg} style={{ width: "100%", height: "50%" }} />
+    </div>
+  );
+};
+
+export default Cars;
---
+++ frontend/src/utils/movement.jsx
@@ -0,0 +1,70 @@
+export const isBetween = (val, curr, prev) =>
+  (val <= curr && val >= prev) || (val >= curr && val <= prev);
+
+export const getNextCoordIndex = (currX, currY, path) => {
+  return path.findIndex(([x, y], i) => {
+    if (currX === path[i][0] && currY === path[i][1]) return true;
+    if (i === 0) return false;
+
+    const xMatches = x === currX;
+    const yMatches = y === currY;
+
+    return (
+      (xMatches && isBetween(currY, path[i][1], path[i - 1][1])) ||
+      (yMatches && isBetween(currX, path[i][0], path[i - 1][0]))
+    );
+  });
+};
+
+export const advanceCoord = (curr, next, increment) => {
+  if (next > curr) {
+    curr = curr + increment;
+    if (curr + increment > next) curr = next;
+  } else {
+    curr = curr - increment;
+    if (curr - increment < next) curr = next;
+  }
+
+  return curr;
+};
+
+const getDirection = (section, i) => {
+  // console.log(section)
+  console.log(section)
+  const x0 = section[0][0];
+  const x1 = section[i][0];
+  x1 === x0 ? "y" : "x";
+};
+
+export const countTurns = (section) => {
+  let count = 0;
+  let currentDirection = getDirection(section, 1);
+
+  for (let i = 0; i < section.length; i++) {
+    let newDirection = getDirection(section, i);
+    if (newDirection !== currentDirection) {
+      currentDirection = newDirection;
+      count++;
+    }
+  }
+  return count;
+};
+
+export const finalRotation = (path, i) => {
+  const [x0, y0] = path[i - 1];
+  const [x1, y1] = path[i - 1];
+  const direction = x1 === x0 ? "y" : "x";
+
+  if (direction === "x" && x1 > x0) return 90;
+  else if (direction === "x" && x0 > x1) return 270;
+  else if (direction === "y" && y1 > y0) return 180;
+  else return 0;
+};
+
+export const getTurnDistance = (curr, target) => {
+  return {
+    Clockwise: curr && target <= 360 ? target - curr : 360 - curr + target,
+    AntiClockwise:
+      target >= 0 && target < curr ? curr - target : curr + 360 - target,
+  };
+};
